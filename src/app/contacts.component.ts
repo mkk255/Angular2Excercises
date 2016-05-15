277c65bceab7432aa4262008da6f85ebbdce157b
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-05-15 12:48:03 -0700

    step 1: use ContactsHeaderComponent as a child component

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ContactsHeaderComponent } from './contacts-header';
 
 @Component({
   moduleId: module.id,
   selector: 'trm-contacts-app',
   templateUrl: 'contacts.component.html',
-  styleUrls: ['contacts.component.css']
+  styleUrls: ['contacts.component.css'],
+  directives: [ContactsHeaderComponent]
 })
 export class ContactsAppComponent {
-  title = 'Angular 2 Master Class setup works!';
+
 }
