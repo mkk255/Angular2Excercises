5f1a9400c240c17914ab1df3fbaaf23a39a85810
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-05-15 22:27:36 -0700

    step 2: display first contact

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ContactsHeaderComponent } from './contacts-header';
+import { Contact } from './models/contact';
 
 @Component({
   moduleId: module.id,
@@ -10,4 +11,19 @@ import { ContactsHeaderComponent } from './contacts-header';
 })
 export class ContactsAppComponent {
 
+  contact: Contact = {
+    id: 7,
+    name: 'Diana Ellis',
+    email: '',
+    phone: '',
+    birthday: '',
+    website: '',
+    image: '/assets/images/6.jpg',
+    address: {
+      street: '6554 park lane',
+      zip: '43378',
+      city: 'Rush',
+      country: 'United States'
+    }
+  };
 }
