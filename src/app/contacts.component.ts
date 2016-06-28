4785a1774c4971827cd4536572751993602a81f9
Pascal Precht <pascal.precht@googlemail.com>
2016-06-28 11:41:07 +0200

    step 4: introduce ContactsService and ngOnInit

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ContactsHeaderComponent } from './contacts-header';
 import { Contact } from './models/contact';
-import { CONTACT_DATA } from './data/contact-data';
+import { ContactsService } from './contacts.service';
 
 @Component({
   moduleId: module.id,
   selector: 'trm-contacts-app',
   templateUrl: 'contacts.component.html',
   styleUrls: ['contacts.component.css'],
-  directives: [ContactsHeaderComponent]
+  directives: [ContactsHeaderComponent],
+  providers: [ContactsService]
 })
-export class ContactsAppComponent {
+export class ContactsAppComponent implements OnInit {
 
-  contacts: Array<Contact> = CONTACT_DATA;
+  contacts: Array<Contact>;
+
+  constructor(private contactsService: ContactsService) {}
+
+  ngOnInit () {
+    this.contacts = this.contactsService.getContacts();
+  }
 }
