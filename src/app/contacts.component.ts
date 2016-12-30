bcdb29ff106a5c5fb9a6105c0ebbf2e6b95829a1
Pascal Precht <pascal.precht@googlemail.com>
2016-12-30 12:22:04 +0100

    step 1: use MdToolbar as a child component to render header

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'trm-contacts-app',
-  templateUrl: './contacts.component.html',
+  template: `
+    <md-toolbar color="primary">Contacts</md-toolbar>
+  `,
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsAppComponent {
