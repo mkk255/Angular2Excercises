a551867d20efd576269292e9d6951247a9504441
Pascal Precht <pascal.precht@googlemail.com>
2017-01-02 16:19:31 +0100

    step 5: move logic into search service

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -4,12 +4,7 @@ import { Subject } from 'rxjs/Subject';
 import { Contact } from '../models/contact';
 import { ContactsService } from '../contacts.service';
 
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/merge';
-import 'rxjs/add/operator/delay';
-import 'rxjs/add/operator/takeUntil';
 
 @Component({
   selector: 'trm-contacts-list',
@@ -24,10 +19,8 @@ export class ContactsListComponent implements OnInit {
   constructor(private contactsService: ContactsService) {}
 
   ngOnInit () {
-    this.contacts = this.terms$.debounceTime(400)
-                               .distinctUntilChanged()
-                               .switchMap(term => this.contactsService.search(term))
-                               .merge(this.contactsService.getContacts().delay(5000).takeUntil(this.terms$))
+    this.contacts = this.contactsService.search(this.terms$)
+                                        .merge(this.contactsService.getContacts());
   }
 
   trackByContactId(index, contact) {
