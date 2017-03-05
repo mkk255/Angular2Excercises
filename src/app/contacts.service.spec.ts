5428d0f4a4b12c502f4375b05cff44d6dd65e754
Thomas Burleson <ThomasBurleson@Gmail.com>
2017-03-05 09:52:32 -0600

    step 5a: ContactsService using fake backend: refactor and cleanup

--- src/app/contacts.service.spec.ts
+++ src/app/contacts.service.spec.ts
@@ -1,142 +1,140 @@
-import { TestBed, inject, async } from '@angular/core/testing';
+import {TestBed, inject, async} from '@angular/core/testing';
 
-import { Inject } from '@angular/core';
-import { HttpModule, XHRBackend, RequestMethod, Response } from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
+import {HttpModule, XHRBackend, RequestMethod, Response, ResponseOptions} from '@angular/http';
+import {MockBackend} from '@angular/http/testing';
 
-import { ContactsService } from './contacts.service';
-import { API_ENDPOINT } from './app.tokens';
+import {API_ENDPOINT} from './app.tokens';
+import {ContactsService} from './contacts.service';
 
-describe('ContactsService', () => {
+xdescribe('ContactsService with live RESTful services', () => {
+  let contactsService: ContactsService;
 
-
-  xdescribe('using real backend', () => {
-
-    let contactsService: ContactsService;
-
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        providers: [
-          ContactsService,
-          { provide: API_ENDPOINT, useValue: 'http://localhost:4201/api' }
-        ],
-        imports: [HttpModule]
-      });
-    });
-
-    beforeEach(inject([ContactsService], (service) => {
-      contactsService = service;
-    }));
-
-    describe('getContacts()', () => {
-
-      it('should fetch and emit contacts list', async(() => {
-        contactsService.getContacts().subscribe(contacts => {
-          expect(contacts.length).toEqual(11);
-          expect(contacts[0].name).toEqual('Christoph Burgdorf');
-          expect(contacts[1].name).toEqual('Pascal Precht');
-          expect(contacts[2].name).toEqual('Nicole Hansen');
-        });
-      }));
-    });
-
-    describe('getContact()', () => {
-
-      it('should fetch and emit single contact by given id', async(() => {
-        contactsService.getContact('0').subscribe(contact => {
-          expect(contact).toBeDefined();
-          expect(contact.id).toEqual(0);
-          expect(contact.name).toEqual('Christoph Burgdorf');
-        });
-      }));
+  /**
+   * !! Developers should run `npm run rest-api` to ensure the app server is available
+   * for these live RESTful service calls.
+   */
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        ContactsService,
+        {provide: API_ENDPOINT, useValue: 'http://localhost:4201/api'}
+      ],
+      imports: [HttpModule]
     });
   });
+  beforeEach(inject([ContactsService], (service) => {
+    contactsService = service;
+  }));
 
-  describe('using fake backend', () => {
+  describe('for contact lookups', () => {
 
-    let contactsService: ContactsService;
-    let backend: XHRBackend;
-
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        providers: [
-          ContactsService,
-          { provide: API_ENDPOINT, useValue: 'http://localhost:4201/api' },
-          { provide: XHRBackend, useClass: MockBackend }
-        ]
-        imports: [HttpModule]
+    /**
+     * Note `async()` required for live RESTful service calls
+     */
+    it('should fetch and emit contacts list', async(() => {
+      contactsService.getContacts().subscribe(contacts => {
+        expect(contacts.length).toEqual(11);
+        expect(contacts[0].name).toEqual('Christoph Burgdorf');
+        expect(contacts[1].name).toEqual('Pascal Precht');
+        expect(contacts[2].name).toEqual('Nicole Hansen');
       });
-    });
-
-    beforeEach(inject([ContactsService, XHRBackend], (service, mockBackend) => {
-      contactsService = service;
-      backend = mockBackend;
     }));
 
-    describe('getContacts()', () => {
-
-      it('should fetch and emit contacts array', () => {
-
-        let mockResponse = {
-          items: [
-            { id: 0, name: 'First contact' },
-            { id: 1, name: 'Second contact' },
-            { id: 2, name: 'Third contact' }
-          ]
-        };
-
-        backend.connections.subscribe(connection => {
-          connection.mockRespond(new Response({ body: JSON.stringify(mockResponse) }));
-        });
-
-        contactsService.getContacts().subscribe(contacts => {
-          expect(contacts.length).toEqual(3);
-          expect(contacts[0].name).toEqual('First contact');
-          expect(contacts[1].name).toEqual('Second contact');
-          expect(contacts[2].name).toEqual('Third contact');
-        });
+    it('should fetch and emit single contact by given id', async(() => {
+      contactsService.getContact('0').subscribe(contact => {
+        expect(contact).toBeDefined();
+        expect(contact.id).toEqual(0);
+        expect(contact.name).toEqual('Christoph Burgdorf');
       });
-    });
+    }));
 
-    describe('getContact()', () => {
-
-      it('should fetch and emit single contact by given id', () => {
-        let mockResponse = {
-          item: { id: 0, name: 'First contact' }
-        };
-
-        backend.connections.subscribe(connection => {
-          connection.mockRespond(new Response({ body: JSON.stringify(mockResponse) }));
-        });
-
-        contactsService.getContact('0').subscribe(contact => {
-          expect(contact).toBeDefined();
-          expect(contact.id).toEqual(0);
-          expect(contact.name).toEqual('First contact');
-        });
-      });
-    });
-
-    describe('updateContact()', () => {
-
-      it('should update existing contact', () => {
-        let mockResponse = {
-          item: { id: 0, name: 'Other name' }
-        };
-
-        backend.connections.subscribe(connection => {
-          // As we're in full control over what MockBackend returns, the only
-          // thing we're really interested in is if a correct request, with the
-          // correct payload is made.
-          expect(connection.request.url).toBe('http://localhost:4201/api/contacts/0');
-          expect(connection.request.method).toBe(RequestMethod.Put);
-          expect(JSON.parse(connection.request.getBody())).toEqual(mockResponse.item);
-          connection.mockRespond(new Response({ body: JSON.stringify(mockResponse) }));
-        });
-
-        contactsService.updateContact(mockResponse.item).subscribe();
-      });
-    });
   });
+
 });
 
+describe('ContactsService using Mock HTTP services', () => {
+  let contactsService: ContactsService;
+  let backend: XHRBackend;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpModule],
+      providers: [
+        ContactsService,
+        {provide: API_ENDPOINT, useValue: 'http://localhost:4201/api'},
+        {provide: XHRBackend, useClass: MockBackend}
+      ]
+    });
+  });
+  beforeEach(inject([ContactsService, XHRBackend], (service, mockBackend) => {
+    contactsService = service;
+    backend = mockBackend;
+  }));
+
+  describe('for contact lookups', () => {
+
+    it('should fetch and emit contacts list', async(() => {
+      let mockResponse = {
+        items: [
+          {id: 0, name: 'First contact'},
+          {id: 1, name: 'Second contact'},
+          {id: 2, name: 'Third contact'}
+        ]
+      };
+      backend.connections.subscribe(connection => {
+        connection.mockRespond( new Response( new ResponseOptions({
+          body: JSON.stringify(mockResponse)
+        })));
+      });
+
+      contactsService.getContacts().subscribe(contacts => {
+        expect(contacts.length).toEqual(3);
+        expect(contacts[0].name).toEqual('First contact');
+        expect(contacts[1].name).toEqual('Second contact');
+        expect(contacts[2].name).toEqual('Third contact');
+      });
+    }));
+
+    it('should fetch and emit single contact by given id', async(() => {
+      backend.connections.subscribe(connection => {
+        connection.mockRespond( new Response( new ResponseOptions({
+            body: JSON.stringify({
+              item: {id: 0, name: 'First contact'}
+            })
+        })));
+      });
+
+      contactsService.getContact('0').subscribe(contact => {
+        expect(contact).toBeDefined();
+        expect(contact.id).toEqual(0);
+        expect(contact.name).toEqual('First contact');
+      });
+    }));
+  });
+
+  describe('for contact modifications', () => {
+
+    it('should update existing contact', () => {
+      let mockResponse = {
+        item: {id: 0, name: 'Other name'}
+      };
+
+      // As we're in full control over what MockBackend returns, the only
+      // thing we're really interested in is if a correct request, with the
+      // correct payload is made.
+      backend.connections.subscribe(connection => {
+        expect(connection.request.url).toBe('http://localhost:4201/api/contacts/0');
+        expect(connection.request.method).toBe(RequestMethod.Put);
+        expect(JSON.parse(connection.request.getBody())).toEqual(mockResponse.item);
+
+        connection.mockRespond( new Response( new ResponseOptions({
+          body: JSON.stringify(mockResponse)
+        })));
+      });
+
+      let subscription = contactsService.updateContact(mockResponse.item).subscribe();
+      subscription.unsubscribe();
+    });
+  });
+
+});
