f8d301c41a692e601e33f98b4710f06af3ff86bd
Thomas Burleson <ThomasBurleson@Gmail.com>
2017-03-05 13:36:21 -0600

    step 10: Using testing helpers
    * build API intercept that has `respondWith()` method
    * use `createFixture()` & `queryFor()` helper functions
    * demonstrate bug and test fail with queryFor.

--- src/app/contacts-list/contacts-list.component.spec.ts
+++ src/app/contacts-list/contacts-list.component.spec.ts
@@ -1,51 +1,59 @@
-import { TestBed, ComponentFixture } from '@angular/core/testing';
-
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { By } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
 
-import { ContactsListComponent } from './contacts-list.component';
-import { ContactsService } from '../contacts.service';
-import { EventBusService } from '../event-bus.service';
-import { API_ENDPOINT } from '../app.tokens';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {TestBed, inject} from '@angular/core/testing';
+
+import {ContactsListComponent} from './contacts-list.component';
+import {ContactsService} from '../contacts.service';
+import {EventBusService} from '../event-bus.service';
+import {API_ENDPOINT} from '../app.tokens';
+
+// Load External Test Helper functions
+import {createFixture, queryFor} from '../utils/testing/helpers';
+import {DebugElement} from '@angular/core';
 
 describe('ContactsListComponent', () => {
-
-  let fixture: ComponentFixture<ContactsListComponent>;
+  let contactsService: ContactsService;
+  let interceptAPI;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [HttpModule],
       declarations: [ContactsListComponent],
       providers: [
-        ContactsService,
-        EventBusService,
-        { provide: API_ENDPOINT, useValue: 'http://localhost:4201/api' }
-      ],
-      imports: [HttpModule],
-      schemas: [NO_ERRORS_SCHEMA]
+        ContactsService, EventBusService,
+        {provide: API_ENDPOINT, useValue: 'http://localhost:4201/api'}
+      ]
     });
-
-    fixture = TestBed.createComponent(ContactsListComponent);
   });
+  beforeEach(inject([ContactsService], (_contactsService) => {
+    // Build partial application to work with spyOn and intercept a service call
+    interceptAPI = (method) => {
+      let respondWith = (data) => {
+        spyOn(_contactsService, method).and.returnValue(Observable.of(data));
+      };
+      return { respondWith };
+    };
+    contactsService = _contactsService;
+  }));
 
   it('should fetch and display contacts', () => {
-    let contactsService = fixture.debugElement.injector.get(ContactsService);
+    interceptAPI('getContacts').respondWith([
+      {id: 0, name: 'First contact', image: '/assets/images/1.jpg'},
+      {id: 1, name: 'Second contact', image: '/assets/images/2.jpg'}
+    ]);
 
-    spyOn(contactsService, 'getContacts').and.returnValue(Observable.of([
-      { id: 0, name: 'First contact', image: '/assets/images/1.jpg' }
-      { id: 1, name: 'Second contact', image: '/assets/images/2.jpg' }
-    ]));
-
-    fixture.detectChanges();
-
-    let viewItems = fixture.debugElement.queryAll(By.css('h3'));
+    let fixture = createFixture(ContactsListComponent);
+    let items: DebugElement[] = queryFor(fixture, 'h3');
 
     expect(contactsService.getContacts).toHaveBeenCalled();
-    expect(viewItems.length).toEqual(2);
-    expect(viewItems[0].nativeElement.textContent).toEqual('First contact');
-    expect(viewItems[1].nativeElement.textContent).toEqual('Second contact');
+    expect(items.length).toEqual(2);
+
+    // These will fail, use DevTools to debug and find out why
+    expect(items[0].textContent).toEqual('First contact');
+    expect(items[1].textContent).toEqual('Second contact');
   });
 });
